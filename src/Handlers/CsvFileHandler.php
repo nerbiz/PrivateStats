4a3b52938fc0dfec0ad7fe61af5129910249bec0
Sybren Dijkstra <nerbiz@gmail.com>
2019-10-13 17:27:19 +0200

    Implemented storing statistics in a CSV file

--- src/Handlers/CsvFileHandler.php
+++ src/Handlers/CsvFileHandler.php
@@ -2,6 +2,7 @@
 
 namespace Nerbiz\PrivateStats\Handlers;
 
+use Jenssegers\Date\Date;
 use Nerbiz\PrivateStats\VisitInfo;
 
 class CsvFileHandler extends AbstractFileHandler implements HandlerInterface
@@ -11,6 +12,29 @@ class CsvFileHandler extends AbstractFileHandler implements HandlerInterface
      */
     public function store(VisitInfo $visitInfo): bool
     {
-        return true;
+        // (Try to) create the file, if it doesn't exist yet
+        if (! file_exists($this->filePath)) {
+            $csvHeader = implode(',', ['IP hash', 'URL', 'Referring URL', 'Timestamp', 'Date']);
+            $fileIsCreated = file_put_contents($this->filePath, $csvHeader . PHP_EOL, FILE_APPEND);
+
+            if ($fileIsCreated === false) {
+                return false;
+            }
+        }
+
+        // Create the CSV values, escape possible commas, by using quotes
+        $csvValues = [
+            $visitInfo->getIpHash(),
+            '"' . $visitInfo->getUrl() . '"',
+            '"' . $visitInfo->getReferringUrl() . '"',
+            $visitInfo->getTimestamp(),
+            Date::createFromTimestamp($visitInfo->getTimestamp())->format('Y-m-d H:i:s'),
+        ];
+
+        // Add a row to the file
+        $csvRow = implode(',', $csvValues);
+        $rowIsAdded = file_put_contents($this->filePath, $csvRow . PHP_EOL, FILE_APPEND);
+
+        return ($rowIsAdded !== false);
     }
 }
